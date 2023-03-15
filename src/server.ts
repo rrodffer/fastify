98ece4227b5473ce62f7bda5cf5cd72609a014f0
rrodffer <rferdev@gmail.com>
2023-03-14 22:59:33 -0300

    Feat: add configuracoes de lint com eslint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,8 +6,10 @@ app.get('/teste', () => {
   return 'FASTIFY 🥸'
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('Servidor iniciado 🕷️')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Servidor iniciado 🕷️')
+  })
